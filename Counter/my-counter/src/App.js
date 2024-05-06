cc334e30ac6b8932c58d4eda4263b4197d065869
5030ravirao07@gmail.com <5030ravirao07@gmail.com>
2024-05-06 20:44:08 +0530

    re update

--- Counter/my-counter/src/App.js
+++ Counter/my-counter/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Counter</h1>
+        <Counter/>
       </header>
     </div>
   );
